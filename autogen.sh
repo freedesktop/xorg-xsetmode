fba8c4faa965cb385e41c9032f98e973a03b2b25
Mihail Konev <k.mvc@ya.ru>
2017-01-26 14:00:21 +1000

    autogen: add default patch prefix
    Signed-off-by: Mihail Konev <k.mvc@ya.ru>

--- autogen.sh
+++ autogen.sh
@@ -9,5 +9,8 @@ cd $srcdir
 autoreconf -v --install || exit 1
 cd $ORIGDIR || exit $?
 
+git config --local --get format.subjectPrefix >/dev/null 2>&1 ||
+    git config --local format.subjectPrefix "PATCH app/xsetmode"
+
 $srcdir/configure --enable-maintainer-mode "$@"
 
